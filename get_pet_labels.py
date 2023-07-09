843f665c0ec864cb9e8587378e32383904927bbf
Mamillapalli Jyothika <109164392+Jyo2253@users.noreply.github.com>
2023-07-09 22:16:00 +0530

    Update get_pet_labels.py

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -5,7 +5,7 @@ from os import listdir
 #       in the return statement with results_dic dictionary that you create 
 #       with this function
 # 
-def get_pet_labels(image_dir):
+def get_pet_labels(img_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
     of the image files. These pet image labels are used to check the accuracy 
@@ -15,14 +15,14 @@ def get_pet_labels(image_dir):
     and with leading and trailing whitespace characters stripped from them.
     (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
     Parameters:
-     image_dir - The (full) path to the folder of images that are to be
+     img_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
       results_dic - Dictionary with 'key' as image filename and 'value' as a 
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    in_files = listdir(image_dir)
+    in_files = listdir(img_dir)
     results_dic = dict()
 
     for idx in range(0, len(in_files), 1):
