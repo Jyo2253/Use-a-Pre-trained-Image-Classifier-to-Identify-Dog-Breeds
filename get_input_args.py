afb32939fc6f5fefb378d80892a35ba0ba51bc1e
Mamillapalli Jyothika <109164392+Jyo2253@users.noreply.github.com>
2023-07-12 18:39:04 +0530

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
+#                                                                             
+# PROGRAMMER: M.Jyothika
+# DATE CREATED:   07/10/2023                                
+# REVISED DATE: 
+# PURPOSE: Create a function that retrieves the following 3 command line inputs 
+#          from the user using the Argparse Python module. If the user fails to 
+#          provide some or all of the 3 inputs, then the default values are
+#          used for the missing inputs. Command Line Arguments:
+#     1. Image Folder as --dir with default value 'pet_images'
+#     2. CNN Model Architecture as --arch with default value 'vgg'
+#     3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
+#
+##
 # Imports python modules
 import argparse
 
@@ -23,13 +39,19 @@ def get_input_args():
      parse_args() -data structure that stores the command line arguments object  
     """
     # Create Parse using ArgumentParser
-    parser = argparse.ArgumentParser()
     
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    parser.add_argument('--dir', type=str, default = 'pet_images', help='path to image folder')
-    parser.add_argument('--arch', type=str, default = 'vgg', help='CNN Model Architecture')
-    parser.add_argument('--dogfile', type=str, default = 'dognames.txt', help='text file with dog names')
+    
     
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', default = 'pet_images' )
+    #parser.add_argument('--dogfile', default = 'dognames.txt' )
+    parser.add_argument('--arch', default = 'vgg' )
+    parser.add_argument('--dogfile', default = 'dognames.txt' )
+    
+    
+    
     return parser.parse_args()
